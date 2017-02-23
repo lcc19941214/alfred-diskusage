11d912e4340f2832103ce856d0cedd9c56a87daa
lcc19941214 <245987239@qq.com>
2017-02-23 18:56:01 +0800

    feat: 1.1.0
    1. support copying info from list items and show notifications.
    
    Change-Id: I3edeeeb3e296982cc86fc326453e73d5ba2f4db0

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const lines = info.split('\n');
 const contents = lines.map(line => line.split(/\s+/));
 const diskinfo = contents.filter(line => /disk(?!\w+)?/g.test(line[0]));
 const items = diskinfo.map(info => ({
-  title: `Size: ${info[1]}   Used: ${info[2]}  Avail: ${info[3]}`,
-  subtitle: info[0]
+  title: `Size: ${info[1]}  Used: ${info[2]}  Avail: ${info[3]}`,
+  subtitle: info[0],
+  arg: `Size: ${info[1]}  Used: ${info[2]}  Avail: ${info[3]}`
 }));
 
 alfy.output(items);
