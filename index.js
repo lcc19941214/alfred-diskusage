fc0549a59a3ea9ed8ca45ca142baa7a1ede89a17
lcc19941214 <245987239@qq.com>
2017-02-28 15:28:13 +0800

    feat: v0.2.0
    1. reversion this package
    2. show all disk in mac instead of disk name matching 'desk.*'
    
    Change-Id: I21b21c6131b98f6ad88007cc43b3d311ec18ca71

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ config.silent = true;
 const info = exec('df -h').stdout;
 const lines = info.split('\n');
 const contents = lines.map(line => line.split(/\s+/));
-const diskinfo = contents.filter(line => /disk(?!\w+)?/g.test(line[0]));
+const diskinfo = contents.slice(1).filter(line => line.every(v => v !== '' && v !== undefined && v !== null));
+diskinfo.sort((a, b) => a[0].localeCompare(b[0]));
 const items = diskinfo.map(info => ({
   title: `Size: ${info[1]}  Used: ${info[2]}  Avail: ${info[3]}`,
   subtitle: info[0],
